20b9be198e120cf71456a8d768584c5b93898f82
Zeusz77 <aron.franta@gmail.com>
2021-09-13 14:17:18 +0200

    +1 megoldás variáns

--- 2021-22-1/nappali-7/gyak2/script2.js
+++ 2021-22-1/nappali-7/gyak2/script2.js
@@ -4,7 +4,7 @@ for(let i = 10; i < 100; i+=10){
     let bekezdes = document.createElement('p')
     bekezdes.innerHTML = szoveg
     bekezdes.style.fontSize = `${i}px`
-    bekezdes.style.color = `#13${i}4B`
+    bekezdes.style.color = `#FF${i}FF`
     
     document.body.appendChild(bekezdes)
 }
